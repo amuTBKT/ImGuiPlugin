22d3accb7f12ae138382f8dd78f563ae72f19a12
Amit Kumar Mehar <gookutbkt@gmail.com>
2022-05-29 21:18:34 +0530

    Added ImGui as submodule

--- Source/ImGuiPlugin.Build.cs
+++ Source/ImGuiPlugin.Build.cs
@@ -1,45 +1,62 @@
 using System.IO;
+using UnrealBuildTool;
 
-namespace UnrealBuildTool.Rules
+public class ImGuiPlugin : ModuleRules
 {
-	public class ImGuiPlugin : ModuleRules
+	public ImGuiPlugin(ReadOnlyTargetRules Target) : base(Target)
 	{
-		public ImGuiPlugin(ReadOnlyTargetRules Target) : base(Target)
-		{
-			PublicDefinitions.Add("WITH_IMGUI=1");
+		PublicDefinitions.Add("WITH_IMGUI=1");
 
-			PublicIncludePaths.AddRange(
+		PublicIncludePaths.AddRange(
+			new string[]
+			{
+				Path.Combine(ModuleDirectory, "../Thirdparty/ImGui/imgui"),
+			}
+		);
+
+		if (Target.Configuration == UnrealTargetConfiguration.Debug)
+		{
+			PublicAdditionalLibraries.AddRange(
 				new string[]
 				{
-					Path.Combine(ModuleDirectory, "Public/imgui"),
+					Path.Combine(ModuleDirectory, "../Thirdparty/ImGui/libs/Debug/ImGui.lib"),
 				}
 			);
+		}
+		else
+        {
+			PublicAdditionalLibraries.AddRange(
+				new string[]
+				{
+					Path.Combine(ModuleDirectory, "../Thirdparty/ImGui/libs/Release/ImGui.lib"),
+				}
+			);
+		}
 
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"Slate",
+				"SlateCore",
+			}
+		);
+
+		if (Target.bBuildEditor)
+		{
 			PrivateDependencyModuleNames.AddRange(
 				new string[]
 				{
-					"Core",
-					"CoreUObject",
-					"Engine",
-					"InputCore",
-					"Slate",
-					"SlateCore",
+					"UnrealEd",
+					"EditorStyle",
+					"Blutility",
+					"SourceControl",
+					"UncontrolledChangelists",
 				}
 			);
-
-			if (Target.bBuildEditor)
-			{
-				PrivateDependencyModuleNames.AddRange(
-					new string[]
-					{
-						"UnrealEd",
-						"EditorStyle",
-						"Blutility",
-						"SourceControl",
-						"UncontrolledChangelists",
-					}
-				);
-			}
 		}
 	}
 }
---
+++ Thirdparty/ImGui/Build.bat
@@ -0,0 +1,4 @@
+cmake . -B Intermediate
+
+"D:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe" Intermediate/ImGui.sln /p:Configuration=Debug
+"D:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe" Intermediate/ImGui.sln /p:Configuration=Release
