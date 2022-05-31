cab64507c5af9c8a1d2ac10862e5d8e6246270f9
Amit Kumar Mehar <gookutbkt@gmail.com>
2022-05-31 18:55:51 +0530

    Fixed image size issue

--- Source/Private/ImGuiPluginModule.cpp
+++ Source/Private/ImGuiPluginModule.cpp
@@ -160,7 +160,7 @@ FImGuiImageBindParams FImGuiPluginModule::RegisterOneFrameResource(const FSlateB
 	const FVector2f SizeUV = Proxy->SizeUV;
 
 	FImGuiImageBindParams Params = {};
-	Params.Size = ImVec2(SlateBrush->ImageSize.X, SlateBrush->ImageSize.Y);
+	Params.Size = ImVec2(LocalSize.X, LocalSize.Y);
 	Params.UV0 = ImVec2(StartUV.X, StartUV.Y);
 	Params.UV1 = ImVec2(StartUV.X + SizeUV.X, StartUV.Y + SizeUV.Y);
 	Params.Id = IndexToImGuiID(NewIndex);
