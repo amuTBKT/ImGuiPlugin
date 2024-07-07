f4138a1a920e71e7054936a2bc76a52b10a22506
Amit Kumar Mehar <gookutbkt@gmail.com>
2024-07-07 09:24:52 +0530

    Renamed file

---
+++ Source/ImGuiRuntime/Public/ImGuiStaticWidget.h
@@ -0,0 +1,30 @@
+// Copyright 2024 Amit Kumar Mehar. All Rights Reserved.
+
+#pragma once
+
+#include "ImGuiSubsystem.h"
+
+// Static widgets are registered at ModuleInit, so we cannot use logic that relies on initialization order.
+
+#define IMGUI_REGISTER_STATIC_WIDGET(InitFunction, TickFunction)					\
+struct FAtModuleInit																\
+{																					\
+    FAtModuleInit()																	\
+    {																				\
+		if (UImGuiSubsystem* Subsystem = UImGuiSubsystem::Get())					\
+		{																			\
+			InitFunction();															\
+			Subsystem->GetMainWindowTickDelegate().AddStatic(&TickFunction);		\
+		}																			\
+        else																		\
+		{																			\
+			UImGuiSubsystem::OnSubsystemInitialized().AddLambda(					\
+			[](UImGuiSubsystem* Subsystem)											\
+			{																		\
+				InitFunction();														\
+				Subsystem->GetMainWindowTickDelegate().AddStatic(&TickFunction);	\
+			});																		\
+        }																			\
+    }																				\
+};																					\
+static FAtModuleInit AtModuleInit = {};
