5aaed98b7297689b98b7885a0002108b272158cb
Amit Kumar Mehar <gookutbkt@gmail.com>
2025-12-21 11:51:21 +0530

    Moved widget registration helpers to module file

--- Source/ImGuiRuntime/Private/ImGuiRuntimeModule.cpp
+++ Source/ImGuiRuntime/Private/ImGuiRuntimeModule.cpp
@@ -19,6 +19,84 @@
 
 #define LOCTEXT_NAMESPACE "ImGuiPlugin"
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FAutoRegisterMainWindowWidget::FAutoRegisterMainWindowWidget(FStaticWidgetRegisterParams RegisterParams)
+{
+	if (!ensureAlways(RegisterParams.IsValid()))
+	{
+		return;
+	}
+
+	if (UImGuiSubsystem* ImGuiSubsystem = UImGuiSubsystem::Get())
+	{
+		RegisterParams.InitFunction();
+		ImGuiSubsystem->GetMainWindowTickDelegate().AddStatic(RegisterParams.TickFunction);
+	}
+	else
+	{
+		UImGuiSubsystem::OnSubsystemInitialized().AddLambda(
+			[RegisterParams](UImGuiSubsystem* ImGuiSubsystem)
+			{
+				RegisterParams.InitFunction();
+				ImGuiSubsystem->GetMainWindowTickDelegate().AddStatic(RegisterParams.TickFunction);
+			});
+	}
+}
+
+#if WITH_EDITOR
+TSharedRef<FWorkspaceItem> GetImGuiTabGroup();
+
+static TSharedRef<SDockTab> SpawnWidgetTab(const FSpawnTabArgs& SpawnTabArgs, FStaticWidgetRegisterParams RegisterParams)
+{
+	FOnTickImGuiWidgetDelegate TickDelegate;
+	TickDelegate.BindStatic(RegisterParams.TickFunction);
+
+	return SNew(SDockTab)
+		.TabRole(ETabRole::NomadTab)
+		[
+			SNew(SImGuiWidget)
+			.MainViewportWindow(SpawnTabArgs.GetOwnerWindow())
+			.OnTickDelegate(TickDelegate)
+			.ConfigFileName(RegisterParams.WidgetName)
+			.bEnableViewports(RegisterParams.bEnableViewports)
+		];
+}
+
+FAutoRegisterStandaloneWidget::FAutoRegisterStandaloneWidget(FStaticWidgetRegisterParams RegisterParams)
+{
+	if (!ensureAlways(RegisterParams.IsValid()))
+	{
+		return;
+	}
+
+	if (UImGuiSubsystem* ImGuiSubsystem = UImGuiSubsystem::Get())
+	{
+		RegisterParams.InitFunction();
+		FGlobalTabmanager::Get()->RegisterNomadTabSpawner(FName(RegisterParams.WidgetName), FOnSpawnTab::CreateStatic(&SpawnWidgetTab, RegisterParams))
+			.SetGroup(GetImGuiTabGroup())
+			.SetDisplayName(FText::FromString(ANSI_TO_TCHAR(RegisterParams.WidgetName)))
+			.SetTooltipText(FText::FromString(ANSI_TO_TCHAR(RegisterParams.WidgetDescription)))
+			.SetIcon(RegisterParams.WidgetIcon);
+	}
+	else
+	{
+		UImGuiSubsystem::OnSubsystemInitialized().AddLambda(
+			[RegisterParams](UImGuiSubsystem* ImGuiSubsystem)
+			{
+				RegisterParams.InitFunction();
+				FGlobalTabmanager::Get()->RegisterNomadTabSpawner(FName(RegisterParams.WidgetName), FOnSpawnTab::CreateStatic(&SpawnWidgetTab, RegisterParams))
+					.SetGroup(GetImGuiTabGroup())
+					.SetDisplayName(FText::FromString(ANSI_TO_TCHAR(RegisterParams.WidgetName)))
+					.SetTooltipText(FText::FromString(ANSI_TO_TCHAR(RegisterParams.WidgetDescription)))
+					.SetIcon(RegisterParams.WidgetIcon);
+			});
+	}
+}
+#endif //#if WITH_EDITOR
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class FImGuiRuntimeModule : public IModuleInterface
 {
 private:
