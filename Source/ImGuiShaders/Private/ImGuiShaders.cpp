b637877cb34705c955e3c22d19d598096c3cf4eb
Amit Kumar Mehar <gookutbkt@gmail.com>
2026-01-13 20:22:11 +0530

    Moved shader implementation to module file.

--- Source/ImGuiShaders/Private/ImGuiShaders.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2024-25 Amit Kumar Mehar. All Rights Reserved.
-
-#include "ImGuiShaders.h"
-
-IMPLEMENT_GLOBAL_SHADER(FImGuiVS, "/Plugin/ImGui/ImGuiShader.usf", "MainVS", SF_Vertex);
-
-IMPLEMENT_GLOBAL_SHADER(FImGuiPS, "/Plugin/ImGui/ImGuiShader.usf", "MainPS", SF_Pixel);
--- Source/ImGuiShaders/Private/ImGuiShadersModule.cpp
+++ Source/ImGuiShaders/Private/ImGuiShadersModule.cpp
@@ -1,26 +1,26 @@
 // Copyright 2024-25 Amit Kumar Mehar. All Rights Reserved.
 
-#include "CoreMinimal.h"
-#include "Modules/ModuleInterface.h"
-#include "Modules/ModuleManager.h"
-#include "Interfaces/IPluginManager.h"
 #include "Misc/Paths.h"
 #include "ShaderCore.h"
+#include "ImGuiShaders.h"
+#include "Modules/ModuleManager.h"
+#include "Modules/ModuleInterface.h"
+#include "Interfaces/IPluginManager.h"
+
+IMPLEMENT_GLOBAL_SHADER(FImGuiVS, "/Plugin/ImGui/ImGuiShader.usf", "MainVS", SF_Vertex);
+IMPLEMENT_GLOBAL_SHADER(FImGuiPS, "/Plugin/ImGui/ImGuiShader.usf", "MainPS", SF_Pixel);
 
 class FImGuiShadersModule : public IModuleInterface
 {
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+	virtual void StartupModule() override
+	{
+		const FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("ImGuiPlugin"))->GetBaseDir(), TEXT("Shaders"));
+		AddShaderSourceDirectoryMapping(TEXT("/Plugin/ImGui"), PluginShaderDir);
+	}
+	
+	virtual void ShutdownModule() override
+	{
+	}
 };
 
 IMPLEMENT_MODULE(FImGuiShadersModule, ImGuiShaders)
-
-void FImGuiShadersModule::StartupModule()
-{
-	const FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("ImGuiPlugin"))->GetBaseDir(), TEXT("Shaders"));
-	AddShaderSourceDirectoryMapping(TEXT("/Plugin/ImGui"), PluginShaderDir);
-}
-
-void FImGuiShadersModule::ShutdownModule()
-{
-}
